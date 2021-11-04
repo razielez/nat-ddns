59eaec80a84ae9a2db5b2b3c17b6f19160548b35
razielez <linran@live.com>
2021-11-04 17:55:55 +0800

    :sparkles:

---
+++ nat-ddns-common/src/main/java/com/razielez/HeartbeatHandler.java
@@ -0,0 +1,48 @@
+package com.razielez;
+
+import com.razielez.codec.Message;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class HeartbeatHandler extends ChannelInboundHandlerAdapter {
+
+  protected ChannelHandlerContext ctx;
+
+  @Override
+  public void channelActive(ChannelHandlerContext ctx) throws Exception {
+    this.ctx = ctx;
+  }
+
+  @Override
+  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+    log.error("Channel exception caught", cause.getCause());
+  }
+
+  @Override
+  public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+    if (evt instanceof IdleStateEvent) {
+      IdleState state = ((IdleStateEvent) evt).state();
+      switch (state) {
+        case READER_IDLE:
+          processReaderIdle();
+          break;
+        case WRITER_IDLE:
+          processWriterIdle();
+          break;
+      }
+    }
+  }
+
+  private void processReaderIdle() {
+    log.info("Client close, remote: {}", ctx.channel().remoteAddress());
+    ctx.close();
+  }
+
+  private void processWriterIdle() {
+    ctx.writeAndFlush(Message.HEARTBEAT);
+  }
+}
--- nat-ddns-common/src/main/java/com/razielez/codec/Message.java
+++ nat-ddns-common/src/main/java/com/razielez/codec/Message.java
@@ -1,10 +1,16 @@
 package com.razielez.codec;
 
 import java.util.Map;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Message {
+
+  public static final Message HEARTBEAT = new Message(MessageType.HEARTBEAT);
 
   /**
    * 消息流水号
@@ -13,6 +19,10 @@ public class Message {
   private MessageType type;
   private Map<String, Object> mateData;
   private byte[] body;
+
+  public Message(MessageType type) {
+    this.type = type;
+  }
 
   public static Message create(
       final MessageType type,
@@ -25,5 +35,4 @@ public class Message {
     message.setBody(body);
     return message;
   }
-
 }
